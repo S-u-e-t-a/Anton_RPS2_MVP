e8266d347ada1d59ae19b9afeaf21d49cc62abcf
Anton <anton.gusevv.01@gmail.com>
2021-03-07 13:51:26 +0300

    Initial commit

---
+++ Lab2/Lab2/Models/ICipher.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2.Models
+{
+    public interface ICipher
+    {
+        string Text { get; set; }
+
+        string Encode(string text);
+        string Decode(string text);
+    }
+}
---
+++ Lab2/Lab2/Presenters/CipherPresenter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lab2.Views;
+using Lab2.Models;
+
+namespace Lab2.Presenters
+{
+    public class CipherPresenter
+    {
+        ICipher cipherView;
+
+        public CipherPresenter(ICipher view)
+        {
+            cipherView = view;
+        }
+
+        public void Ciphering()
+        {
+            var atbash = new Atbash
+        }
+    }
+}
